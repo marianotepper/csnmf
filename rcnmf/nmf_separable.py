4af58b9c4f4901275dbb7f5f37ae1cae2ea1fdc1
Mariano <marianotepper@gmail.com>
2015-02-02 18:55:23 -0500

    Split line with more than 80 characters

--- rcnmf/nmf_separable.py
+++ rcnmf/nmf_separable.py
@@ -11,41 +11,42 @@ from __future__ import absolute_import
 
 import numpy as np
 from rcnmf.third_party import mrnmf
+import compression
 
 
-def compress(data, n_power_iter=0):
+def compute_compressed(data, q, alg, n_power_iter=0):
 
-    n = data.shape[1]
-
-    omega = np.random.standard_normal(size=(n, n))
-    mat_h = data.dot(omega)
-    for j in range(n_power_iter):
-        mat_h = data.dot(data.T.dot(mat_h))
-    comp = np.linalg.qr(mat_h, 'reduced')[0]
-    comp = comp.T
-
-    data_comp = comp.dot(data)
-
-    return np.linalg.qr(data_comp, 'reduced')[1]
-
-
-def compute(data, q, alg, n_power_iter=0):
-
-    left_factor = compress(data, n_power_iter)
+    data_comp = compression.compress(data, q, n_power_iter)
     colnorms = np.sum(np.fabs(data), axis=0)
 
-    return mrnmf.nmf(left_factor, colnorms, alg, q)
+    return mrnmf.nmf(data_comp, colnorms, alg, q)
+
+
+def compute(data, q, alg):
+
+    data_comp = np.linalg.qr(data)[1]
+    colnorms = np.sum(np.fabs(data), axis=0)
+
+    return mrnmf.nmf(data_comp, colnorms, alg, q)
 
 
 if __name__ == "__main__":
 
-    m = 100
-    n = 50
+    import time
+    np.random.seed(10)
+
+    m = 10000
+    n = 500
     q = 5
 
     x = np.fabs(np.random.standard_normal(size=(m, q)))
     y = np.fabs(np.random.standard_normal(size=(q, n)))
     data = x.dot(y)
 
-    cols, H, rel_res = compute(data, q, 'SPA')
-    print rel_res
+    t = time.clock()
+    cols, H, rel_res = compute_compressed(data, q, 'xray')
+    print cols, rel_res, time.clock() - t
+
+    t = time.clock()
+    cols, H, rel_res = compute(data, q, 'xray')
+    print cols, rel_res, time.clock() - t
