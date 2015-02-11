9b95b4098615713df96f26b6cdba3c8ce7a78bc3
Mariano <marianotepper@gmail.com>
2015-02-10 23:00:02 -0500

    Working implementation of tsqr using dask arrays

--- rcnmf/tsqr.py
+++ rcnmf/tsqr.py
@@ -15,6 +15,8 @@ import into
 from math import ceil
 import matplotlib.pyplot as plt
 import tempfile
+import operator
+
 
 def findnumblocks(shape, blockshape):
 
@@ -24,13 +26,6 @@ def findnumblocks(shape, blockshape):
     return tuple(nb)
 
 
-def tuple_to_outputs(out0, out1, t):
-    # into.into(out0, t[0], inplace=True)
-    # into.into(out1, t[1], inplace=True)
-    np.copyto(out0, t[0])
-    np.copyto(out1, t[1])
-
-
 def create_keys(name, numblocks):
     block_list = map(tuple, map(range, numblocks))
     keys = [[name]]
@@ -38,51 +33,100 @@ def create_keys(name, numblocks):
     return list(product(*keys))
 
 
-def tsqr(mat, blockshape=None):
+def tsqr(data, blockshape=None):
 
-    assert(mat.shape[1] == blockshape[1])
-
-    data = da.from_array(mat, blockshape=blockshape, name='A')
+    m, n = mat.shape
+    assert(n == blockshape[1])
 
     numblocks = findnumblocks(mat.shape, blockshape)
-    dsk_qr = da.core.top(np.linalg.qr, 'QR', 'ij', 'A', 'ij',
-                       numblocks={'A': numblocks})
 
-    kn = mat.shape[1] * numblocks[0]
-    Q_arr = np.zeros(shape=(mat.shape[0], kn))
-    Q = da.from_array(Q_arr, blockshape=blockshape, name='Q')
-    R_arr = np.zeros(shape=(kn, mat.shape[1]))
-    R = da.from_array(R_arr, blockshape=(blockshape[1], blockshape[1]), name='R')
+    dsk_qr_st1 = da.core.top(np.linalg.qr, 'QR_st1', 'ij', 'A', 'ij',
+                             numblocks={'A': numblocks})
+    # qr[0]
+    dsk_q_st1 = {('Q_st1', i, 0): (operator.getitem, ('QR_st1', i, 0), 0)
+                 for i in xrange(numblocks[0])}
+    # qr[1]
+    dsk_r_st1 = {('R_st1', i, 0): (operator.getitem, ('QR_st1', i, 0), 1)
+                 for i in xrange(numblocks[0])}
 
-    numblocks_dict = {'QR': numblocks, 'R': numblocks,
-                      'Q': (numblocks[0], numblocks[0])}
-    dsk = da.core.top(tuple_to_outputs, 'RES', 'ij', 'Q', 'ii', 'R', 'ij',
-                        'QR', 'ij', numblocks=numblocks_dict)
+    def _vstack(*args):
+        tup = tuple(args)
+        # print [t.shape for t in tup]
+        return np.vstack(tup)
 
-    dsk.update(data.dask)
-    dsk.update(dsk_qr)
-    dsk.update(Q.dask)
-    dsk.update(R.dask)
+    to_append = [_vstack] + [('R_st1', i, 0) for i in xrange(numblocks[0])]
+    dsk_r_st1_stacked = {('R_st1_stacked', 0, 0): tuple(to_append)}
 
-    keys = create_keys('RES', numblocks)
-    dask.get(dsk, keys)
+    dsk_qr_st2 = da.core.top(np.linalg.qr, 'QR_st2', 'ij', 'R_st1_stacked', 'ij',
+                             numblocks={'R_st1_stacked': (1, 1)})
+    # qr[0]
+    dsk_q_st2_aux = {('Q_st2_aux', 0, 0): (operator.getitem, ('QR_st2', 0, 0), 0)}
+    dsk_q_st2 = dict((('Q_st2',) + ijk,
+                      (operator.getitem, ('Q_st2_aux', 0, 0),
+                       tuple(slice(i*d, (i+1)*d) for i, d in zip(ijk, (n, n)))))
+                     for ijk in product(*map(range, numblocks)))
+    # qr[1]
+    dsk_r_st2 = {('R', i, 0): (operator.getitem, ('QR_st2', i, 0), 1)
+                 for i in xrange(numblocks[0])}
 
-    Q_st2, R_st2 = np.linalg.qr(R_arr)
+    dsk_q_st3 = da.core.top(np.dot, 'Q', 'ij', 'Q_st1', 'ij', 'Q_st2', 'ij',
+                            numblocks={'Q_st1': numblocks, 'Q_st2': numblocks})
 
-    plt.figure()
-    plt.subplot(2, 3, 1)
-    plt.imshow(Q_arr, interpolation='nearest')
-    plt.subplot(2, 3, 4)
-    plt.imshow(R_arr, interpolation='nearest')
-    plt.subplot(2, 3, 5)
-    plt.imshow(Q_st2, interpolation='nearest')
-    plt.subplot(2, 3, 6)
-    plt.imshow(R_st2, interpolation='nearest')
+    dsk_q = {}
+    dsk_q.update(data.dask)
+    dsk_q.update(dsk_qr_st1)
+    dsk_q.update(dsk_q_st1)
+    dsk_q.update(dsk_r_st1)
+    dsk_q.update(dsk_r_st1_stacked)
+    dsk_q.update(dsk_qr_st2)
+    dsk_q.update(dsk_q_st2_aux)
+    dsk_q.update(dsk_q_st2)
+    dsk_q.update(dsk_q_st3)
+    dsk_r = {}
+    dsk_r.update(data.dask)
+    dsk_r.update(dsk_qr_st1)
+    dsk_r.update(dsk_r_st1)
+    dsk_r.update(dsk_r_st1_stacked)
+    dsk_r.update(dsk_qr_st2)
+    dsk_r.update(dsk_r_st2)
 
-    plt.show()
+    # print dsk_q
+    # print dsk_r
 
+    Q = da.Array(dsk_q, 'Q', shape=mat.shape, blockshape=blockshape)
+    R = da.Array(dsk_r, 'R', shape=(n, n), blockshape=(n, n))
+
+    return Q, R
 
 if __name__ == '__main__':
 
-    mat = np.random.randn(1000, 20)
-    tsqr(mat, blockshape=(250, 20))
+    mat = np.random.rand(100, 20)
+    blockshape=(100, 20)
+    data = da.from_array(mat, blockshape=blockshape, name='A')
+    print data.dask
+    Q, R = tsqr(data, blockshape=blockshape)
+
+    print Q.shape
+    Q = np.array(Q)
+
+    R = np.array(R)
+    print R.shape
+
+    plt.figure()
+    plt.subplot(2, 4, 1)
+    plt.imshow(mat, interpolation='nearest')
+    plt.subplot(2, 4, 2)
+    plt.imshow(Q, interpolation='nearest')
+    plt.subplot(2, 4, 3)
+    plt.imshow(np.dot(Q.T, Q), interpolation='nearest')
+    plt.subplot(2, 4, 4)
+    plt.imshow(R, interpolation='nearest')
+
+    plt.subplot(2, 4, 5)
+    plt.spy(mat)
+    plt.subplot(2, 4, 6)
+    plt.spy(Q)
+    plt.subplot(2, 4, 8)
+    plt.spy(R)
+
+    plt.show()
