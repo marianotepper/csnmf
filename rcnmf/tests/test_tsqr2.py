cc853bf48f69e8506d024bb2299bb76ced49341a
Mariano <marianotepper@gmail.com>
2015-02-12 15:36:38 -0500

    Basic test for tsqr with subsequent multiplications.

---
+++ rcnmf/tests/test_tsqr2.py
@@ -0,0 +1,32 @@
+import dask.array as da
+from into import into
+from dask.array.into import discover
+from dask.dot import dot_graph
+import tempfile
+import rcnmf.tsqr
+
+x = da.random.standard_normal(size=(100, 100), blockshape=(100, 50))
+
+temp_file = tempfile.NamedTemporaryFile(suffix='.hdf5')
+
+uri = temp_file.name + '::/X'
+into(uri, x)
+
+data = into(da.Array, uri, blockshape=(100, 100))
+
+omega = da.random.standard_normal(size=(100, 20), blockshape=(100, 20))
+mat_h = data.dot(omega)
+
+q, r = rcnmf. tsqr.tsqr(mat_h, blockshape=(100, 20))
+
+print data.shape
+print q.shape
+
+mul = data.dot(q)
+
+dot_graph(data.dask, filename='data')
+dot_graph(omega.dask, filename='omega')
+dot_graph(q.dask, filename='q')
+dot_graph(mul.dask, filename='mul')
+
+temp_file.close()
